5e14305223417b3fde4aac357087e65188e92ded
Gav <gav.s.may@gmail.com>
2026-09-12 21:51:55 +0100

    Please review GPT

--- Maths/fractions.py
+++ Maths/fractions.py
@@ -1,12 +1,38 @@
-a_num = 3
-a_den = 4
+print("Enter your first fraction")
 
-b_num = 6
-b_den = 8
+while True:
+    try:
+        a_num = int(input("First Numerator "))
+        a_den = int(input("First Denominator "))
+        b_num = int(input("Second Numerator "))
+        b_den = int(input("Second Denominator "))
+    
+    except ZeroDivisionError:
+        print("Can't Divide by zero")
+    except ValueError:
+        print("A incorrect value was given")
 
-a = a_num / a_den
-b = b_num / b_den
+    break
 
-print(a)
-print(b)
-print(a == b)
+print(f"Fraction A: {a_num} / {a_den}")
+print(f"Fraction B: {b_num} / {b_den}")
+
+frac_a = a_num/a_den
+frac_b = b_num/b_den
+
+fraction_A_greater = frac_a > frac_b
+
+if fraction_A_greater:
+    print(f"{a_num} / {a_den} is Greater than {b_num} / {b_den}")
+else:
+    print(f"{a_num} / {a_den} is Less than {b_num} / {b_den}")
+
+common = a_den * b_den
+print(common)
+first_adder = (common / a_den) * a_num
+second_adder = (common / b_den) * b_num
+sum_adder = int(first_adder + second_adder)
+
+print(f"{sum_adder} / {common}")
+
+ 
